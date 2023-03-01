802d91f47323be20a331e377a50eb1e3eb9cb41d
Brett Byrd <byrdb2@yahoo.com>
2023-02-28 18:26:36 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    name:'brett' ,
+    streetAddress: '123 Rocky Road'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const updateEmployee = {...employee}
+updateEmployee[key] = value
+return updateEmployee
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]= value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+const updateEmployee = {...employee}
+    delete updateEmployee[key]
+return updateEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
